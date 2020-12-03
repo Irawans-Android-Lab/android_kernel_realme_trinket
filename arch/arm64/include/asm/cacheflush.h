98fe28a1e93ae8cf08dc92aaa43b978984da4259
dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
2020-12-04 05:13:17 +0700

    arch: arm64: Clean Import realmeme P-OSS code changes

--- arch/arm64/include/asm/cacheflush.h
+++ arch/arm64/include/asm/cacheflush.h
@@ -168,4 +168,11 @@ static inline void flush_cache_vunmap(unsigned long start, unsigned long end)
 
 int set_memory_valid(unsigned long addr, int numpages, int enable);
 
+#ifdef CONFIG_PRODUCT_REALME_TRINKET
+int set_memory_ro(unsigned long addr, int numpages);
+int set_memory_rw(unsigned long addr, int numpages);
+int set_memory_x(unsigned long addr, int numpages);
+int set_memory_nx(unsigned long addr, int numpages);
+#endif
+
 #endif
--- arch/arm64/mm/fault.c
+++ arch/arm64/mm/fault.c
@@ -48,6 +48,10 @@
 #include <acpi/ghes.h>
 #include <soc/qcom/scm.h>
 #include <trace/events/exception.h>
+#ifdef CONFIG_ODM_WT_EDIT
+// Hui.Wang@ODM_WT.BSP.Kernel.Stability.1941873, 2019/05/31, Add for display boot reason
+#include <wt_sys/wt_boot_reason.h>
+#endif
 
 struct fault_info {
 	int	(*fn)(unsigned long addr, unsigned int esr,
@@ -286,6 +290,13 @@ static void __do_kernel_fault(unsigned long addr, unsigned int esr,
 
 	pr_alert("Unable to handle kernel %s at virtual address %08lx\n", msg,
 		 addr);
+
+#ifdef CONFIG_ODM_WT_EDIT
+// Hui.Wang@ODM_WT.BSP.Kernel.Stability.1941873, 2019/05/31, Add for display boot reason
+#ifdef CONFIG_WT_BOOT_REASON
+	save_panic_key_log("Unable to handle kernel %s at virtual address %08lx\n", msg, addr);
+#endif
+#endif
 
 	mem_abort_decode(esr);
 
