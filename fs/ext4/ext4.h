abhi9960/lord_kurama_trinket

Import realme EXT4_ASYNC_DISCARD from Q-OSS

fs/ext4/ext4.h
@@ -42,6 +42,10 @@
 
 #define __FS_HAS_ENCRYPTION IS_ENABLED(CONFIG_EXT4_FS_ENCRYPTION)
 #include <linux/fscrypt.h>
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+#include "discard.h"
+#endif
 
 /*
  * The fourth extended filesystem constants/structures
@@ -1151,6 +1155,10 @@ struct ext4_inode_info {
 #define EXT4_MOUNT_DIOREAD_NOLOCK	0x400000 /* Enable support for dio read nolocking */
 #define EXT4_MOUNT_JOURNAL_CHECKSUM	0x800000 /* Journal checksums */
 #define EXT4_MOUNT_JOURNAL_ASYNC_COMMIT	0x1000000 /* Journal Async Commit */
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+#define EXT4_MOUNT_ASYNC_DISCARD	0x2000000 /* Async issue discard request */
+#endif
 #define EXT4_MOUNT_DELALLOC		0x8000000 /* Delalloc support */
 #define EXT4_MOUNT_DATA_ERR_ABORT	0x10000000 /* Abort on file data write */
 #define EXT4_MOUNT_BLOCK_VALIDITY	0x20000000 /* Block validity checking */
@@ -1360,6 +1368,39 @@ struct ext4_super_block {
 /* Number of quota types we support */
 #define EXT4_MAXQUOTAS 3
 
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+struct discard_policy {
+	int type;			/* type of discard */
+	unsigned int min_interval;	/* used for candidates exist */
+	unsigned int mid_interval;	/* used for device busy */
+	unsigned int max_interval;	/* used for candidates not exist */
+	unsigned int max_requests;	/* # of discards issued per round */
+	unsigned int io_aware_gran;	/* minimum granularity discard not be aware of I/O */
+	unsigned int granularity;	/* discard granularity */
+	bool io_aware;				/* issue discard in idle time */
+	bool sync;					/* submit discard with REQ_SYNC flag */
+};
+
+struct discard_cmd_control {
+	struct task_struct *ext4_issue_discard_thread;	/* discard thread */
+	wait_queue_head_t discard_wait_queue;	/* waiting queue for wake-up */
+	unsigned int discard_wake;				/* to wake up discard thread */
+	struct mutex cmd_lock;
+	unsigned long long max_discards;		/* max. discards to be issued */
+	unsigned int discard_granularity;		/* discard granularity */
+	unsigned int dpolicy_param_tune;		/* tune dpolicy param*/
+	unsigned long long issued_discard;		/* # of issued discard */
+	ext4_group_t group_start;				/*current trim point, start group num*/
+	ext4_grpblk_t blk_offset;				/*current trim point, group internal block offset*/
+	unsigned long long cnt_io_interrupted;	/* # of interrupted by IO */
+	unsigned int total_trimed_groups;		/* # of total trimed groups */
+	void * groups_block_bitmap;				/* backup trimmed groups block bitmap */
+	struct discard_policy dpolicy;			/* discard policy */
+	bool io_interrupted;					/*if the discard thread interrupted by IO*/
+};
+#endif
+
 /*
  * fourth extended-fs super-block data in memory
  */
@@ -1533,6 +1574,13 @@ struct ext4_sb_info {
 	/* Barrier between changing inodes' journal flags and writepages ops. */
 	struct percpu_rw_semaphore s_journal_flag_rwsem;
 	struct dax_device *s_daxdev;
+/* for discard command control */
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	struct discard_cmd_control *dcc_info;
+	unsigned long last_time;	/* to store time in jiffies */
+	long interval_time;		/* to store thresholds */
+#endif
 };
 
 static inline struct ext4_sb_info *EXT4_SB(struct super_block *sb)
@@ -2965,6 +3013,49 @@ static inline void ext4_unlock_group(struct super_block *sb,
 {
 	spin_unlock(ext4_group_lock_ptr(sb, group));
 }
+
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+static inline int ext4_utilization(struct ext4_sb_info *sbi)
+{
+	return div_u64((u64)ext4_free_blocks_count(sbi->s_es) * 100,
+					ext4_blocks_count(sbi->s_es));
+}
+
+static inline bool ext4_readonly(struct super_block *sb)
+{
+	return sb->s_flags & MS_RDONLY;
+}
+
+static inline void ext4_update_time(struct ext4_sb_info *sbi)
+{
+	sbi->last_time = jiffies;
+}
+
+static inline bool ext4_time_over(struct ext4_sb_info *sbi)
+{
+	struct timespec ts = {sbi->interval_time, 0};
+	unsigned long interval = timespec_to_jiffies(&ts);
+
+	return time_after(jiffies, sbi->last_time + interval);
+}
+
+static inline bool is_ext4_idle(struct ext4_sb_info *sbi)
+{
+	struct block_device *bdev = sbi->s_sb->s_bdev;
+	struct request_queue *q = bdev_get_queue(bdev);
+	struct request_list *rl = &q->root_rl;
+
+	if (rl->count[BLK_RW_SYNC] || rl->count[BLK_RW_ASYNC])
+		return 0;
+
+	return ext4_time_over(sbi);
+}
+
+extern int ext4_trim_groups(struct super_block *sb,  struct discard_cmd_control *dcc);
+extern int create_discard_cmd_control(struct ext4_sb_info *sbi);
+extern void destroy_discard_cmd_control(struct ext4_sb_info *sbi);
+#endif
 
 /*
  * Block validity checking

fs/ext4/inode.c
@@ -1458,6 +1458,11 @@ errout:
 			ext4_orphan_del(NULL, inode);
 	}
 
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	ext4_update_time(EXT4_SB(inode->i_sb));
+#endif
+
 	return ret ? ret : copied;
 }
 
@@ -1574,6 +1579,11 @@ errout:
 		if (inode->i_nlink)
 			ext4_orphan_del(NULL, inode);
 	}
+
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	ext4_update_time(EXT4_SB(inode->i_sb));
+#endif
 
 	return ret ? ret : copied;
 }
@@ -4472,6 +4482,11 @@ int ext4_truncate(struct inode *inode)
 
 	if (inode->i_size & (inode->i_sb->s_blocksize - 1))
 		ext4_block_truncate_page(handle, mapping, inode->i_size);
+
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+	//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	ext4_update_time(EXT4_SB(inode->i_sb));
+#endif
 
 	/*
 	 * We add the inode to the orphan list, so that if this

fs/ext4/ioctl.c
@@ -286,6 +286,10 @@ static int ext4_ioctl_setflags(struct inode *inode,
 	inode->i_ctime = current_time(inode);
 
 	err = ext4_mark_iloc_dirty(handle, inode, &iloc);
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	ext4_update_time(EXT4_SB(inode->i_sb));
+#endif
 flags_err:
 	ext4_journal_stop(handle);
 	if (err)
@@ -929,6 +933,11 @@ resizefs_out:
 		struct request_queue *q = bdev_get_queue(sb->s_bdev);
 		struct fstrim_range range;
 		int ret = 0;
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+		if (test_opt(sb, ASYNC_DISCARD))  
+			return 0;
+#endif
 
 		if (!capable(CAP_SYS_ADMIN))
 			return -EPERM;

fs/ext4/namei.c
@@ -2125,6 +2125,10 @@ out:
 	brelse(bh);
 	if (retval == 0)
 		ext4_set_inode_state(inode, EXT4_STATE_NEWENTRY);
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	ext4_update_time(EXT4_SB(inode->i_sb));
+#endif
 	return retval;
 }
 
@@ -2375,6 +2379,10 @@ static int ext4_delete_entry(handle_t *handle,
 		goto out;
 
 	BUFFER_TRACE(bh, "call ext4_handle_dirty_metadata");
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	ext4_update_time(EXT4_SB(dir->i_sb));
+#endif
 	err = ext4_handle_dirty_dirent_node(handle, dir, bh);
 	if (unlikely(err))
 		goto out;

fs/ext4/sysfs.c
@@ -33,6 +33,10 @@ typedef enum {
 	ptr_explicit,
 	ptr_ext4_sb_info_offset,
 	ptr_ext4_super_block_offset,
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2019-01-08 add for ext4 async discard suppot
+	ptr_discard_cmd_control_offset,
+#endif
 } attr_ptr_t;
 
 static const char proc_dirname[] = "fs/ext4";
@@ -151,6 +155,12 @@ static struct ext4_attr ext4_attr_##_name = {			\
 #define EXT4_RW_ATTR_SBI_UI(_name,_elname)	\
 	EXT4_ATTR_OFFSET(_name, 0644, pointer_ui, ext4_sb_info, _elname)
 
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2019-01-08 add for ext4 async discard suppot
+#define EXT4_RW_ATTR_DCC_UI(_name,_elname)	\
+	EXT4_ATTR_OFFSET(_name, 0644, pointer_ui, discard_cmd_control, _elname)
+#endif
+
 #define EXT4_ATTR_PTR(_name,_mode,_id,_ptr) \
 static struct ext4_attr ext4_attr_##_name = {			\
 	.attr = {.name = __stringify(_name), .mode = _mode },	\
@@ -185,6 +195,17 @@ EXT4_RW_ATTR_SBI_UI(warning_ratelimit_interval_ms, s_warning_ratelimit_state.int
 EXT4_RW_ATTR_SBI_UI(warning_ratelimit_burst, s_warning_ratelimit_state.burst);
 EXT4_RW_ATTR_SBI_UI(msg_ratelimit_interval_ms, s_msg_ratelimit_state.interval);
 EXT4_RW_ATTR_SBI_UI(msg_ratelimit_burst, s_msg_ratelimit_state.burst);
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2019-01-08 add for ext4 async discard suppot
+EXT4_RW_ATTR_DCC_UI(DCC_dpolicy_param_tune, dpolicy_param_tune);
+EXT4_RW_ATTR_DCC_UI(DCC_discard_granularity, discard_granularity);
+EXT4_RW_ATTR_DCC_UI(Dpolicy_min_interval, dpolicy.min_interval);
+EXT4_RW_ATTR_DCC_UI(Dpolicy_mid_interval, dpolicy.mid_interval);
+EXT4_RW_ATTR_DCC_UI(Dpolicy_max_interval, dpolicy.max_interval);
+EXT4_RW_ATTR_DCC_UI(Dpolicy_io_aware_gran, dpolicy.io_aware_gran);
+EXT4_RW_ATTR_DCC_UI(Dpolicy_current_discard_gran, dpolicy.granularity);
+EXT4_RW_ATTR_DCC_UI(Dpolicy_max_requests, dpolicy.max_requests);
+#endif
 EXT4_RO_ATTR_ES_UI(errors_count, s_error_count);
 EXT4_RO_ATTR_ES_UI(first_error_time, s_first_error_time);
 EXT4_RO_ATTR_ES_UI(last_error_time, s_last_error_time);
@@ -217,6 +238,17 @@ static struct attribute *ext4_attrs[] = {
 	ATTR_LIST(errors_count),
 	ATTR_LIST(first_error_time),
 	ATTR_LIST(last_error_time),
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2019-01-08 add for ext4 async discard suppot
+	ATTR_LIST(DCC_dpolicy_param_tune),
+	ATTR_LIST(DCC_discard_granularity),
+	ATTR_LIST(Dpolicy_min_interval),
+	ATTR_LIST(Dpolicy_mid_interval),
+	ATTR_LIST(Dpolicy_max_interval),
+	ATTR_LIST(Dpolicy_io_aware_gran),
+	ATTR_LIST(Dpolicy_current_discard_gran),
+	ATTR_LIST(Dpolicy_max_requests),
+#endif
 	NULL,
 };
 
@@ -249,6 +281,15 @@ static void *calc_ptr(struct ext4_attr *a, struct ext4_sb_info *sbi)
 		return (void *) (((char *) sbi) + a->u.offset);
 	case ptr_ext4_super_block_offset:
 		return (void *) (((char *) sbi->s_es) + a->u.offset);
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2019-01-08 add for ext4 async discard suppot
+	case ptr_discard_cmd_control_offset:
+		if (!test_opt(sbi->s_buddy_cache->i_sb, ASYNC_DISCARD)){
+            return 0;    
+        }
+
+        return (void *) (((char *) sbi->dcc_info) + a->u.offset);
+#endif
 	}
 	return NULL;
 }
@@ -383,6 +424,11 @@ static const struct file_operations ext4_seq_##name##_fops = { \
 PROC_FILE_SHOW_DEFN(es_shrinker_info);
 PROC_FILE_SHOW_DEFN(options);
 
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+PROC_FILE_SHOW_DEFN(discard_info);
+#endif
+
 static const struct ext4_proc_files {
 	const char *name;
 	const struct file_operations *fops;
@@ -390,6 +436,10 @@ static const struct ext4_proc_files {
 	PROC_FILE_LIST(options),
 	PROC_FILE_LIST(es_shrinker_info),
 	PROC_FILE_LIST(mb_groups),
+#if defined(VENDOR_EDIT) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	PROC_FILE_LIST(discard_info),
+#endif
 	{ NULL, NULL },
 };
 

kernel/sysctl.c
@@ -133,6 +133,11 @@ static int one_thousand = 1000;
 #ifdef CONFIG_SCHED_WALT
 static int two_million = 2000000;
 #endif
+#ifdef VENDOR_EDIT
+/*jason.tang@TECH.BSP.Kernel.Storage, 2019-05-20, add control ext4 fsync*/
+unsigned int sysctl_ext4_fsync_enable = 1;
+unsigned int ext4_fsync_enable_status = 0;
+#endif /*VENDOR_EDIT*/
 #ifdef CONFIG_PRINTK
 static int ten_thousand = 10000;
 #endif
@@ -419,6 +424,16 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= sched_updown_migrate_handler,
 	},
+#ifdef VENDOR_EDIT
+/*jason.tang@TECH.BSP.Kernel.Storage, 2019-05-20, add control ext4 fsync*/
+{
+		.procname	= "ext4_fsync_enable",
+		.data		= &sysctl_ext4_fsync_enable,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0666,
+		.proc_handler	= proc_dointvec,
+},
+#endif
 #ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname	= "sched_min_granularity_ns",

Irawans-Android-Lab/android_kernel_realme_trinket

fs/ext4/ext4.h
@@ -42,6 +42,10 @@
 
 #define __FS_HAS_ENCRYPTION IS_ENABLED(CONFIG_EXT4_FS_ENCRYPTION)
 #include <linux/fscrypt.h>
+#if defined(CONFIG_PRODUCT_REALME_TRINKET) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+#include "discard.h"
+#endif
 
 /*
  * The fourth extended filesystem constants/structures
@@ -1154,6 +1158,10 @@ struct ext4_inode_info {
 #define EXT4_MOUNT_DIOREAD_NOLOCK	0x400000 /* Enable support for dio read nolocking */
 #define EXT4_MOUNT_JOURNAL_CHECKSUM	0x800000 /* Journal checksums */
 #define EXT4_MOUNT_JOURNAL_ASYNC_COMMIT	0x1000000 /* Journal Async Commit */
+#if defined(CONFIG_PRODUCT_REALME_TRINKET) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+#define EXT4_MOUNT_ASYNC_DISCARD	0x2000000 /* Async issue discard request */
+#endif
 #define EXT4_MOUNT_DELALLOC		0x8000000 /* Delalloc support */
 #define EXT4_MOUNT_DATA_ERR_ABORT	0x10000000 /* Abort on file data write */
 #define EXT4_MOUNT_BLOCK_VALIDITY	0x20000000 /* Block validity checking */
@@ -1363,6 +1371,39 @@ struct ext4_super_block {
 /* Number of quota types we support */
 #define EXT4_MAXQUOTAS 3
 
+#if defined(CONFIG_PRODUCT_REALME_TRINKET) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+struct discard_policy {
+	int type;			/* type of discard */
+	unsigned int min_interval;	/* used for candidates exist */
+	unsigned int mid_interval;	/* used for device busy */
+	unsigned int max_interval;	/* used for candidates not exist */
+	unsigned int max_requests;	/* # of discards issued per round */
+	unsigned int io_aware_gran;	/* minimum granularity discard not be aware of I/O */
+	unsigned int granularity;	/* discard granularity */
+	bool io_aware;				/* issue discard in idle time */
+	bool sync;					/* submit discard with REQ_SYNC flag */
+};
+
+struct discard_cmd_control {
+	struct task_struct *ext4_issue_discard_thread;	/* discard thread */
+	wait_queue_head_t discard_wait_queue;	/* waiting queue for wake-up */
+	unsigned int discard_wake;				/* to wake up discard thread */
+	struct mutex cmd_lock;
+	unsigned long long max_discards;		/* max. discards to be issued */
+	unsigned int discard_granularity;		/* discard granularity */
+	unsigned int dpolicy_param_tune;		/* tune dpolicy param*/
+	unsigned long long issued_discard;		/* # of issued discard */
+	ext4_group_t group_start;				/*current trim point, start group num*/
+	ext4_grpblk_t blk_offset;				/*current trim point, group internal block offset*/
+	unsigned long long cnt_io_interrupted;	/* # of interrupted by IO */
+	unsigned int total_trimed_groups;		/* # of total trimed groups */
+	void * groups_block_bitmap;				/* backup trimmed groups block bitmap */
+	struct discard_policy dpolicy;			/* discard policy */
+	bool io_interrupted;					/*if the discard thread interrupted by IO*/
+};
+#endif
+
 /*
  * fourth extended-fs super-block data in memory
  */
@@ -1539,6 +1580,13 @@ struct ext4_sb_info {
 	 */
 	struct percpu_rw_semaphore s_writepages_rwsem;
 	struct dax_device *s_daxdev;
+/* for discard command control */
+#if defined(CONFIG_PRODUCT_REALME_TRINKET) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	struct discard_cmd_control *dcc_info;
+	unsigned long last_time;	/* to store time in jiffies */
+	long interval_time;		/* to store thresholds */
+#endif
 };
 
 static inline struct ext4_sb_info *EXT4_SB(struct super_block *sb)
@@ -3004,6 +3052,49 @@ static inline void ext4_unlock_group(struct super_block *sb,
 {
 	spin_unlock(ext4_group_lock_ptr(sb, group));
 }
+
+#if defined(CONFIG_PRODUCT_REALME_TRINKET) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+static inline int ext4_utilization(struct ext4_sb_info *sbi)
+{
+	return div_u64((u64)ext4_free_blocks_count(sbi->s_es) * 100,
+					ext4_blocks_count(sbi->s_es));
+}
+
+static inline bool ext4_readonly(struct super_block *sb)
+{
+	return sb->s_flags & MS_RDONLY;
+}
+
+static inline void ext4_update_time(struct ext4_sb_info *sbi)
+{
+	sbi->last_time = jiffies;
+}
+
+static inline bool ext4_time_over(struct ext4_sb_info *sbi)
+{
+	struct timespec ts = {sbi->interval_time, 0};
+	unsigned long interval = timespec_to_jiffies(&ts);
+
+	return time_after(jiffies, sbi->last_time + interval);
+}
+
+static inline bool is_ext4_idle(struct ext4_sb_info *sbi)
+{
+	struct block_device *bdev = sbi->s_sb->s_bdev;
+	struct request_queue *q = bdev_get_queue(bdev);
+	struct request_list *rl = &q->root_rl;
+
+	if (rl->count[BLK_RW_SYNC] || rl->count[BLK_RW_ASYNC])
+		return 0;
+
+	return ext4_time_over(sbi);
+}
+
+extern int ext4_trim_groups(struct super_block *sb,  struct discard_cmd_control *dcc);
+extern int create_discard_cmd_control(struct ext4_sb_info *sbi);
+extern void destroy_discard_cmd_control(struct ext4_sb_info *sbi);
+#endif
 
 /*
  * Block validity checking

fs/ext4/inode.c
@@ -1486,6 +1486,11 @@ errout:
 			ext4_orphan_del(NULL, inode);
 	}
 
+#if defined(CONFIG_PRODUCT_REALME_TRINKET) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	ext4_update_time(EXT4_SB(inode->i_sb));
+#endif
+
 	return ret ? ret : copied;
 }
 
@@ -1602,6 +1607,11 @@ errout:
 		if (inode->i_nlink)
 			ext4_orphan_del(NULL, inode);
 	}
+
+#if defined(CONFIG_PRODUCT_REALME_TRINKET) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	ext4_update_time(EXT4_SB(inode->i_sb));
+#endif
 
 	return ret ? ret : copied;
 }
@@ -4521,6 +4531,11 @@ int ext4_truncate(struct inode *inode)
 
 	if (inode->i_size & (inode->i_sb->s_blocksize - 1))
 		ext4_block_truncate_page(handle, mapping, inode->i_size);
+
+#if defined(CONFIG_PRODUCT_REALME_TRINKET) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+	//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	ext4_update_time(EXT4_SB(inode->i_sb));
+#endif
 
 	/*
 	 * We add the inode to the orphan list, so that if this

fs/ext4/ioctl.c
@@ -327,6 +327,10 @@ static int ext4_ioctl_setflags(struct inode *inode,
 	inode->i_ctime = current_time(inode);
 
 	err = ext4_mark_iloc_dirty(handle, inode, &iloc);
+#if defined(CONFIG_PRODUCT_REALME_TRINKET) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	ext4_update_time(EXT4_SB(inode->i_sb));
+#endif
 flags_err:
 	ext4_journal_stop(handle);
 	if (err)
@@ -974,6 +978,11 @@ resizefs_out:
 		struct request_queue *q = bdev_get_queue(sb->s_bdev);
 		struct fstrim_range range;
 		int ret = 0;
+#if defined(CONFIG_PRODUCT_REALME_TRINKET) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+		if (test_opt(sb, ASYNC_DISCARD))  
+			return 0;
+#endif
 
 		if (!capable(CAP_SYS_ADMIN))
 			return -EPERM;

fs/ext4/namei.c
@@ -2165,6 +2165,10 @@ out:
 	brelse(bh);
 	if (retval == 0)
 		ext4_set_inode_state(inode, EXT4_STATE_NEWENTRY);
+#if defined(CONFIG_PRODUCT_REALME_TRINKET) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	ext4_update_time(EXT4_SB(inode->i_sb));
+#endif
 	return retval;
 }
 
@@ -2415,6 +2419,10 @@ static int ext4_delete_entry(handle_t *handle,
 		goto out;
 
 	BUFFER_TRACE(bh, "call ext4_handle_dirty_metadata");
+#if defined(CONFIG_PRODUCT_REALME_TRINKET) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	ext4_update_time(EXT4_SB(dir->i_sb));
+#endif
 	err = ext4_handle_dirty_dirent_node(handle, dir, bh);
 	if (unlikely(err))
 		goto out;

fs/ext4/sysfs.c
@@ -33,6 +33,10 @@ typedef enum {
 	ptr_explicit,
 	ptr_ext4_sb_info_offset,
 	ptr_ext4_super_block_offset,
+#if defined(CONFIG_PRODUCT_REALME_TRINKET) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2019-01-08 add for ext4 async discard suppot
+	ptr_discard_cmd_control_offset,
+#endif
 } attr_ptr_t;
 
 static const char proc_dirname[] = "fs/ext4";
@@ -151,6 +155,12 @@ static struct ext4_attr ext4_attr_##_name = {			\
 #define EXT4_RW_ATTR_SBI_UI(_name,_elname)	\
 	EXT4_ATTR_OFFSET(_name, 0644, pointer_ui, ext4_sb_info, _elname)
 
+#if defined(CONFIG_PRODUCT_REALME_TRINKET) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2019-01-08 add for ext4 async discard suppot
+#define EXT4_RW_ATTR_DCC_UI(_name,_elname)	\
+	EXT4_ATTR_OFFSET(_name, 0644, pointer_ui, discard_cmd_control, _elname)
+#endif
+
 #define EXT4_ATTR_PTR(_name,_mode,_id,_ptr) \
 static struct ext4_attr ext4_attr_##_name = {			\
 	.attr = {.name = __stringify(_name), .mode = _mode },	\
@@ -185,6 +195,17 @@ EXT4_RW_ATTR_SBI_UI(warning_ratelimit_interval_ms, s_warning_ratelimit_state.int
 EXT4_RW_ATTR_SBI_UI(warning_ratelimit_burst, s_warning_ratelimit_state.burst);
 EXT4_RW_ATTR_SBI_UI(msg_ratelimit_interval_ms, s_msg_ratelimit_state.interval);
 EXT4_RW_ATTR_SBI_UI(msg_ratelimit_burst, s_msg_ratelimit_state.burst);
+#if defined(CONFIG_PRODUCT_REALME_TRINKET) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2019-01-08 add for ext4 async discard suppot
+EXT4_RW_ATTR_DCC_UI(DCC_dpolicy_param_tune, dpolicy_param_tune);
+EXT4_RW_ATTR_DCC_UI(DCC_discard_granularity, discard_granularity);
+EXT4_RW_ATTR_DCC_UI(Dpolicy_min_interval, dpolicy.min_interval);
+EXT4_RW_ATTR_DCC_UI(Dpolicy_mid_interval, dpolicy.mid_interval);
+EXT4_RW_ATTR_DCC_UI(Dpolicy_max_interval, dpolicy.max_interval);
+EXT4_RW_ATTR_DCC_UI(Dpolicy_io_aware_gran, dpolicy.io_aware_gran);
+EXT4_RW_ATTR_DCC_UI(Dpolicy_current_discard_gran, dpolicy.granularity);
+EXT4_RW_ATTR_DCC_UI(Dpolicy_max_requests, dpolicy.max_requests);
+#endif
 EXT4_RO_ATTR_ES_UI(errors_count, s_error_count);
 EXT4_RO_ATTR_ES_UI(first_error_time, s_first_error_time);
 EXT4_RO_ATTR_ES_UI(last_error_time, s_last_error_time);
@@ -217,6 +238,17 @@ static struct attribute *ext4_attrs[] = {
 	ATTR_LIST(errors_count),
 	ATTR_LIST(first_error_time),
 	ATTR_LIST(last_error_time),
+#if defined(CONFIG_PRODUCT_REALME_TRINKET) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2019-01-08 add for ext4 async discard suppot
+	ATTR_LIST(DCC_dpolicy_param_tune),
+	ATTR_LIST(DCC_discard_granularity),
+	ATTR_LIST(Dpolicy_min_interval),
+	ATTR_LIST(Dpolicy_mid_interval),
+	ATTR_LIST(Dpolicy_max_interval),
+	ATTR_LIST(Dpolicy_io_aware_gran),
+	ATTR_LIST(Dpolicy_current_discard_gran),
+	ATTR_LIST(Dpolicy_max_requests),
+#endif
 	NULL,
 };
 
@@ -249,6 +281,15 @@ static void *calc_ptr(struct ext4_attr *a, struct ext4_sb_info *sbi)
 		return (void *) (((char *) sbi) + a->u.offset);
 	case ptr_ext4_super_block_offset:
 		return (void *) (((char *) sbi->s_es) + a->u.offset);
+#if defined(CONFIG_PRODUCT_REALME_TRINKET) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2019-01-08 add for ext4 async discard suppot
+	case ptr_discard_cmd_control_offset:
+		if (!test_opt(sbi->s_buddy_cache->i_sb, ASYNC_DISCARD)){
+            return 0;    
+        }
+
+        return (void *) (((char *) sbi->dcc_info) + a->u.offset);
+#endif
 	}
 	return NULL;
 }
@@ -383,6 +424,11 @@ static const struct file_operations ext4_seq_##name##_fops = { \
 PROC_FILE_SHOW_DEFN(es_shrinker_info);
 PROC_FILE_SHOW_DEFN(options);
 
+#if defined(CONFIG_PRODUCT_REALME_TRINKET) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+PROC_FILE_SHOW_DEFN(discard_info);
+#endif
+
 static const struct ext4_proc_files {
 	const char *name;
 	const struct file_operations *fops;
@@ -390,6 +436,10 @@ static const struct ext4_proc_files {
 	PROC_FILE_LIST(options),
 	PROC_FILE_LIST(es_shrinker_info),
 	PROC_FILE_LIST(mb_groups),
+#if defined(CONFIG_PRODUCT_REALME_TRINKET) && defined(CONFIG_EXT4_ASYNC_DISCARD_SUPPORT)
+//yh@PSW.BSP.Storage.EXT4, 2018-11-26 add for ext4 async discard suppot
+	PROC_FILE_LIST(discard_info),
+#endif
 	{ NULL, NULL },
 };
 

kernel/sysctl.c
@@ -135,6 +135,11 @@ static int one_thousand = 1000;
 #ifdef CONFIG_SCHED_WALT
 static int two_million = 2000000;
 #endif
+#ifdef CONFIG_PRODUCT_REALME_TRINKET
+/*jason.tang@TECH.BSP.Kernel.Storage, 2019-05-20, add control ext4 fsync*/
+unsigned int sysctl_ext4_fsync_enable = 1;
+unsigned int ext4_fsync_enable_status = 0;
+#endif /*CONFIG_PRODUCT_REALME_TRINKET*/
 #ifdef CONFIG_PRINTK
 static int ten_thousand = 10000;
 #endif
@@ -439,6 +444,16 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= sched_updown_migrate_handler,
 	},
+#ifdef CONFIG_PRODUCT_REALME_TRINKET
+/*jason.tang@TECH.BSP.Kernel.Storage, 2019-05-20, add control ext4 fsync*/
+{
+		.procname	= "ext4_fsync_enable",
+		.data		= &sysctl_ext4_fsync_enable,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0666,
+		.proc_handler	= proc_dointvec,
+},
+#endif
 #ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname	= "sched_min_granularity_ns",